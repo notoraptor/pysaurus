53d7e6733b6622f8311970ad02fd3c762cf374bd
notoraptor <stevenbocco@gmail.com>
2019-09-28 14:30:09 -0400

    Remove: - outdated web interface - unused resource folder.

--- pysaurus/interface/web/src/components/logo.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-export class Logo extends React.Component {
-	render() {
-		return <div className="logo d-flex flex-column justify-content-center">&#120529;s</div>;
-	}
-}
