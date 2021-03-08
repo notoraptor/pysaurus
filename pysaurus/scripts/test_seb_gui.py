db41fd060d3be2f13dd0fcc020c41f1fc21b5302
notoraptor <stevenbocco@gmail.com>
2021-03-08 12:41:39 -0500

    wip

---
+++ pysaurus/scripts/test_seb_gui.py
@@ -0,0 +1,128 @@
+from pysaurus.core.sebgui import HTML, seb_gui, file_path_to_url_path
+import sys
+from pysaurus.core.components import AbsolutePath
+from typing import List, Tuple
+
+
+class Interface:
+    def __init__(self, duplicates: List[Tuple[AbsolutePath, List[AbsolutePath]]]):
+        self._duplicates = duplicates
+        self._gen = HTML(css="""
+        body {
+            font-family: Verdana, sans-serif;
+        }
+        .items {
+            display: flex;
+            flex-direction: row;
+            /* flex-wrap: wrap; */
+            text-align: center;
+            margin-bottom: 0.5rem;
+        }
+        .item {
+            margin-left: 1rem;
+            background-color: rgb(250, 250, 250);
+            border-radius: 5px;
+            padding: 5px;
+        }
+        img {
+            max-height: 100px;
+        }
+        td {
+            vertical-align: top;
+        }
+        td.folder, .item .title {
+            white-space: nowrap;
+        }
+        .item .title {
+            font-size: 0.75rem;
+        }
+        """)
+
+    def _dup_to_html(self, dup):
+        dup_folder, dup_files = dup
+        name = dup_folder.title
+        img_strings = []
+        for i, file_path in enumerate(dup_files):
+            img_strings.append(f"""
+            <div class="item">
+                <div class="image">
+                    <label for="{name}-{i}"><img src="{file_path_to_url_path(file_path.path)}" alt="bad image"/></label>
+                </div>
+                <div class="title">
+                    <input id="{name}-{i}" type="radio" name="{name}" value="{file_path}" {"checked" if i == 0 else ""}/>
+                    <label for="{name}-{i}">{file_path.title}.{file_path.extension}</label>
+                </div>
+            </div>
+            """)
+        return f"""
+        <tr>
+            <td class="folder">
+            <input id="{name}-none" type="radio" name="{name}" value=""/>
+            <label for="{name}-none">{name}</label>
+            </td>
+            <td><div class="items">{"".join(img_strings)}</div></td>
+        </tr>
+        """
+
+    def index(self):
+        return self._gen(
+            f"""
+            <h1>{len(self._duplicates)} duplicate(s)</h1>
+            <form action="app://move" method="post">
+                <table><tbody>
+                {"".join(map(self._dup_to_html, self._duplicates))}
+                </tbody></table>
+                <p>
+                    Output folder:
+                    <input type="text" name="output"/>
+                    <input type="submit" value="send"/>
+                </p>
+            </form>
+            """
+        )
+
+    def move(self, **kwargs):
+        output = None
+        inputs = []
+        for k, v in kwargs.items():
+            if v:
+                path = AbsolutePath(v)
+                if path.isfile():
+                    inputs.append(path)
+                else:
+                    assert path.isdir() or not path.exists()
+                    assert k == "output", (k, v, path)
+                    output = path
+            else:
+                print('Skipped', k)
+        assert output
+        return f"Sent {len(inputs)} file(s) to move to: {output}"
+
+
+def main():
+    folder = AbsolutePath(r'G:\donnees\discord\ero-room\dupplicates')
+    # if len(sys.argv) != 2:
+    #     raise RuntimeError('Required a folder of duplicates.')
+    # folder = AbsolutePath(sys.argv[1])
+    if not folder.isdir():
+        raise RuntimeError(f'Not a folder {folder}')
+    duplicates = []
+    for i, name in enumerate(folder.listdir()):
+        dup_path = AbsolutePath.join(folder, name)
+        files = []
+        if not dup_path.isdir():
+            raise RuntimeError(f'Not a sub-folder: {dup_path}')
+        for sub_name in dup_path.listdir():
+            file_path = AbsolutePath.join(dup_path, sub_name)
+            if not file_path.isfile():
+                raise RuntimeError(f'Not a sub-folder file: {file_path}')
+            files.append(file_path)
+        if len(files) > 1:
+            duplicates.append((dup_path, files))
+        if (i + 1) % 100 == 0:
+            print('Loaded', i + 1)
+    seb_gui("My Software!", Interface(duplicates))
+
+
+if __name__ == "__main__":
+    main()
