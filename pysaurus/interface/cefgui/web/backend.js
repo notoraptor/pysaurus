82ac37e886f0ba3262bb7ae6a03af1aeacb53e1d
notoraptor <notoraptor@users.noreply.github.com>
2021-09-11 17:09:20 -0400

    Remove pywebview GUI.

--- pysaurus/interface/cefgui/web/backend.js
+++
@@ -1,8 +0,0 @@
-/* pywebview backend. */
-window.addEventListener('pywebviewready', function() {
-    console.log("Pywebview loaded.");
-    window.backend_call = function(name, args) {
-        return pywebview.api.call(name, args);
-    };
-    System.import('./build/index.js');
-});
