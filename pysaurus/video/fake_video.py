0bd6bb3dc6bdfbb7c0cc7fc51dbaf158c1fc13e8
notoraptor <notoraptor@users.noreply.github.com>
2023-04-24 20:05:10 -0400

    Make FakeVideo hashable and more similar to Video

--- pysaurus/video/fake_video.py
+++ pysaurus/video/fake_video.py
@@ -1,3 +1,6 @@
+from typing import Any, List
+
+from pysaurus.core.constants import UNDEFINED
 from pysaurus.core.functions import class_get_public_attributes
 
 
@@ -8,8 +11,19 @@ class FakeVideo:
         fields = class_get_public_attributes(type(video))
         self.__dict__.update({field: getattr(video, field) for field in fields})
 
+    def __hash__(self):
+        return hash(self.filename)
+
+    def __eq__(self, other):
+        return self.filename == other.filename
+
+    def __lt__(self, other):
+        return self.filename < other.filename
+
     def has_property(self, name):
         return name in self.properties
 
-    def get_property(self, name, *default):
-        return self.properties.get(name, *default) if default else self.properties[name]
+    def get_property(self, name, default_unit=UNDEFINED) -> List[Any]:
+        return self.properties.get(
+            name, [] if default_unit is UNDEFINED else [default_unit]
+        )
