afc9c65382441f4adae0a9a76dbc40d6b752733f
notoraptor <notoraptor@users.noreply.github.com>
2023-05-20 03:30:16 -0400

    Remove "other" toolsaurus and unused code.

--- other/toolsaurus/other_video/video_runtime_info.py
+++
@@ -1,9 +0,0 @@
-from other.toolsaurus.jsonable import Jsonable
-
-
-class VideoRuntimeInfo(Jsonable):
-    size: "s" = 0
-    mtime: "m" = 0.0
-    driver_id: (int, "d") = None
-    is_file: "f" = False
-    has_thumbnail: "t" = False
