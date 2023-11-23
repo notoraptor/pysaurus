05cb19414c3c8b6e074fd28707a8d26bb6e27ff3
notoraptor <notoraptor@users.noreply.github.com>
2023-11-22 22:01:31 -0500

    update groupable fields definition.

--- saurus/sql/grouping_utils.py
+++ saurus/sql/grouping_utils.py
@@ -1,10 +1,13 @@
+FORMATTED_DURATION_TIME_BASE = "COALESCE(NULLIF(duration_time_base, 0), 1)"
+SQL_LENGTH = f"(duration * 1.0 / {FORMATTED_DURATION_TIME_BASE})"
+
 GROUPABLE_FIELDS = {
     "audio_bit_rate": ["audio_bit_rate"],
     "audio_bits": ["audio_bits"],
     "audio_codec": ["audio_codec"],
     "audio_codec_description": ["audio_codec_description"],
     "bit_rate": [
-        "IIF(duration = 0, 0, file_size * COALESCE(NULLIF(duration_time_base, 0), 1) / duration)"
+        f"IIF(duration = 0, 0, file_size * {FORMATTED_DURATION_TIME_BASE} / duration)"
     ],
     "bit_depth": ["bit_depth"],
     "container_format": ["container_format"],
@@ -12,22 +15,22 @@ GROUPABLE_FIELDS = {
     "date_entry_modified": ["COALESCE(date_entry_modified, mtime)"],
     "date_entry_opened": ["COALESCE(date_entry_opened, mtime)"],
     "day": ["strftime('%Y-%m-%d', datetime(mtime, 'unixepoch'))"],
-    "disk": ["disk"],  # todo
-    "extension": ["extension"],  # todo
+    "disk": ["pysaurus_get_disk(filename, driver_id)"],
+    "extension": ["pysaurus_get_extension(filename)"],
     "file_size": ["file_size"],
-    "file_title": ["file_title"],  # todo
+    "file_title": ["pysaurus_get_file_title(filename)"],
     "file_title_numeric": ["file_title_numeric"],  # todo
     "filename": ["filename"],
     "filename_numeric": ["filename_numeric"],  # todo
     "frame_rate": ["(frame_rate_num * 1.0 / COALESCE(NULLIF(frame_rate_den, 0), 1))"],
     "height": ["height"],
-    "length": ["length"],  # todo
-    "move_id": ["move_id"],  # todo
+    "length": [SQL_LENGTH],
+    "move_id": [f"file_size, {SQL_LENGTH}"],
     "sample_rate": ["sample_rate"],
     "similarity_id": ["similarity_id"],
-    "size": ["size"],  # todo
-    "size_length": ["size_length"],  # todo
-    "title": ["title"],  # todo
+    "size": ["file_size"],
+    "size_length": [f"file_size, {SQL_LENGTH}"],
+    "title": ["pysaurus_get_title(filename, meta_title)"],
     "title_numeric": ["title_numeric"],  # todo
     "video_codec": ["video_codec"],
     "video_codec_description": ["video_codec_description"],
---
+++ saurus/sql/sql_functions.py
@@ -0,0 +1,17 @@
+from pysaurus.core.components import AbsolutePath
+
+
+def pysaurus_get_disk(filename: str, driver_id: int) -> str:
+    return AbsolutePath(filename).get_drive_name() or str(driver_id)
+
+
+def pysaurus_get_extension(filename: str) -> str:
+    return AbsolutePath(filename).extension
+
+
+def pysaurus_get_file_title(filename: str) -> str:
+    return AbsolutePath(filename).file_title
+
+
+def pysaurus_get_title(filename: str, meta_title: str) -> str:
+    return meta_title or AbsolutePath(filename).file_title
