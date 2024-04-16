459414b3563986db52da7c68498dc048f543f872
notoraptor <notoraptor@users.noreply.github.com>
2024-04-16 18:30:50 -0400

    Flet.

--- pysaurus/interface/various/try_flet.py
+++ pysaurus/interface/various/try_flet.py
@@ -1,7 +1,9 @@
-from typing import List
+from typing import List, Set
 
 import flet as ft
 
+from pysaurus.core.components import AbsolutePath
+from pysaurus.core.constants import VIDEO_SUPPORTED_EXTENSIONS
 from pysaurus.core.notifications import Notification
 from pysaurus.interface.api.gui_api import GuiAPI
 
@@ -12,7 +14,13 @@ class Api(GuiAPI):
 
 
 class Homepage(ft.Column):
-    def __init__(self, db_names: List[str]):
+    def __init__(self, page: ft.Page, db_names: List[str]):
+        self.new_paths: Set[AbsolutePath] = set()
+        self.view_selected_paths = ft.Column()
+        files_picker = ft.FilePicker(on_result=self.pick_file)
+        folder_picker = ft.FilePicker(on_result=self.pick_folder)
+        page.overlay.append(files_picker)
+        page.overlay.append(folder_picker)
         super().__init__(
             [
                 ft.Row(
@@ -30,19 +38,35 @@ class Homepage(ft.Column):
                                 ),
                                 ft.Row(
                                     [
-                                        ft.TextButton("Add a file ..."),
-                                        ft.TextButton("Add a folder ..."),
+                                        ft.ElevatedButton(
+                                            "Add files ...",
+                                            icon=ft.icons.VIDEO_FILE,
+                                            on_click=lambda _: files_picker.pick_files(
+                                                dialog_title="Add files ...",
+                                                allow_multiple=True,
+                                                allowed_extensions=sorted(
+                                                    VIDEO_SUPPORTED_EXTENSIONS
+                                                ),
+                                            ),
+                                        ),
+                                        ft.ElevatedButton(
+                                            "Add a folder ...",
+                                            icon=ft.icons.FOLDER_OPEN,
+                                            on_click=lambda _: folder_picker.get_directory_path(
+                                                dialog_title="Add folder ..."
+                                            ),
+                                        ),
                                     ]
                                 ),
-                                ft.Column(),
+                                self.view_selected_paths,
                                 ft.TextButton("Create database"),
-                            ],
+                            ]
                         ),
                         ft.Column(
                             [
                                 ft.Text(f"Open a database ({len(db_names)} available)"),
                                 *[ft.TextButton(name) for name in db_names],
-                            ],
+                            ]
                         ),
                     ],
                     alignment=ft.MainAxisAlignment.CENTER,
@@ -51,6 +75,42 @@ class Homepage(ft.Column):
             ],
             alignment=ft.MainAxisAlignment.CENTER,
         )
+
+    def pick_file(self, e: ft.FilePickerResultEvent):
+        if e.files:
+            self.new_paths.update(AbsolutePath(filename.path) for filename in e.files)
+            self._render_new_paths()
+
+    def pick_folder(self, e: ft.FilePickerResultEvent):
+        if e.path:
+            self.new_paths.add(AbsolutePath(e.path))
+            self._render_new_paths()
+
+    def remove_new_path(self, path: AbsolutePath):
+        if path in self.new_paths:
+            self.new_paths.remove(path)
+            self._render_new_paths()
+
+    def on_remove_path(self, e: ft.ControlEvent):
+        path = e.control.data
+        self.remove_new_path(path)
+
+    def _render_new_paths(self):
+        self.view_selected_paths.controls.clear()
+        for filename in sorted(self.new_paths):
+            self.view_selected_paths.controls.append(
+                ft.Row(
+                    [
+                        ft.IconButton(
+                            icon=ft.icons.DELETE,
+                            on_click=self.on_remove_path,
+                            data=filename
+                        ),
+                        ft.Text(filename.standard_path),
+                    ]
+                )
+            )
+        self.view_selected_paths.update()
 
 
 def main(page: ft.Page):
@@ -61,7 +121,7 @@ def main(page: ft.Page):
     # page.vertical_alignment = ft.MainAxisAlignment.CENTER
     # page.horizontal_alignment = ft.CrossAxisAlignment.CENTER
     page.scroll = ft.ScrollMode.AUTO
-    page.add(Homepage(db_names))
+    page.add(Homepage(page, db_names))
 
 
 if __name__ == "__main__":
