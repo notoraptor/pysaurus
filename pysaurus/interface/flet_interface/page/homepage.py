59cf2252d07c4b395a6d688634e24a4fbdfaf13d
notoraptor <notoraptor@users.noreply.github.com>
2024-04-26 12:08:23 +0100

    Set homepage form.

--- pysaurus/interface/flet_interface/page/homepage.py
+++ pysaurus/interface/flet_interface/page/homepage.py
@@ -1,4 +1,4 @@
-from typing import List, Set
+from typing import List, Optional, Set
 
 import flet as ft
 
@@ -9,10 +9,14 @@ from pysaurus.interface.flet_interface.flet_utils import Title1, Title2
 
 class Homepage(ft.Column):
     def __init__(self, page: ft.Page, db_names: List[str]):
+        self.new_name: str = ""
         self.new_paths: Set[AbsolutePath] = set()
-        self.view_selected_paths = ft.ListView(expand=1)
-        self.files_picker = ft.FilePicker(on_result=self.pick_file)
-        self.folder_picker = ft.FilePicker(on_result=self.pick_folder)
+        self.database_to_load: Optional[str] = None
+        self.update_on_load: bool = False
+
+        self.new_paths_view = ft.ListView(expand=1)
+        self.files_picker = ft.FilePicker(on_result=self.on_result_pick_files)
+        self.folder_picker = ft.FilePicker(on_result=self.on_result_pick_folder)
         page.overlay.append(self.files_picker)
         page.overlay.append(self.folder_picker)
         controls = [
@@ -21,27 +25,40 @@ class Homepage(ft.Column):
             ),
             ft.Column(
                 [
+                    # Header
                     ft.Row(
                         [
                             ft.Column(
                                 [
                                     ft.ElevatedButton(
-                                        content=Title2("Create a database")
+                                        content=Title2("Create a database"),
+                                        on_click=self.on_create_database,
                                     )
                                 ],
                                 expand=1,
                                 horizontal_alignment=ft.CrossAxisAlignment.CENTER,
                             ),
                             ft.Column(
-                                [ft.ElevatedButton(content=Title2(f"Open a database"))],
+                                [
+                                    ft.ElevatedButton(
+                                        content=Title2(f"Open a database"),
+                                        on_click=self.on_open_database,
+                                    )
+                                ],
                                 expand=1,
                                 horizontal_alignment=ft.CrossAxisAlignment.CENTER,
                             ),
                         ]
                     ),
+                    # Database name | Number of available databases
                     ft.Row(
                         [
-                            ft.TextField(label="Database name ...", expand=1),
+                            ft.TextField(
+                                label="Database name ...",
+                                value=self.new_name,
+                                on_change=self.on_change_new_name,
+                                expand=1,
+                            ),
                             ft.Column(
                                 [ft.Text(f"({len(db_names)} available)")],
                                 expand=1,
@@ -49,6 +66,7 @@ class Homepage(ft.Column):
                             ),
                         ]
                     ),
+                    # Database files and folders | update on load
                     ft.Row(
                         [
                             ft.Row(
@@ -68,16 +86,23 @@ class Homepage(ft.Column):
                                 expand=1,
                             ),
                             ft.Row(
-                                [ft.Checkbox("Update on load")],
+                                [
+                                    ft.Checkbox(
+                                        "Update on load",
+                                        value=self.update_on_load,
+                                        on_change=self.on_change_update,
+                                    )
+                                ],
                                 alignment=ft.MainAxisAlignment.CENTER,
                                 expand=1,
                             ),
                         ]
                     ),
+                    # New paths | Available databases
                     ft.Row(
                         [
                             ft.Container(
-                                self.view_selected_paths,
+                                self.new_paths_view,
                                 expand=1,
                                 border_radius=10,
                                 border=ft.border.all(1, ft.colors.GREY),
@@ -86,10 +111,13 @@ class Homepage(ft.Column):
                                 ft.RadioGroup(
                                     ft.ListView(
                                         [
-                                            ft.Radio(value=name, label=name)
+                                            ft.Radio(
+                                                value=name, label=name, toggleable=True
+                                            )
                                             for name in db_names
                                         ]
-                                    )
+                                    ),
+                                    on_change=self.on_database_change,
                                 ),
                                 expand=1,
                                 border_radius=10,
@@ -104,30 +132,20 @@ class Homepage(ft.Column):
         ]
         super().__init__(controls)
 
-    def on_add_files(self, e):
-        self.files_picker.pick_files(
-            dialog_title="Add files ...",
-            allow_multiple=True,
-            allowed_extensions=sorted(VIDEO_SUPPORTED_EXTENSIONS),
-        )
-
-    def on_add_folder(self, e):
-        self.folder_picker.get_directory_path(dialog_title="Add folder ...")
-
-    def pick_file(self, e: ft.FilePickerResultEvent):
+    def on_result_pick_files(self, e: ft.FilePickerResultEvent):
         if e.files:
             self.new_paths.update(AbsolutePath(filename.path) for filename in e.files)
             self.render_new_paths()
 
-    def pick_folder(self, e: ft.FilePickerResultEvent):
+    def on_result_pick_folder(self, e: ft.FilePickerResultEvent):
         if e.path:
             self.new_paths.add(AbsolutePath(e.path))
             self.render_new_paths()
 
     def render_new_paths(self):
-        self.view_selected_paths.controls.clear()
+        self.new_paths_view.controls.clear()
         for filename in sorted(self.new_paths):
-            self.view_selected_paths.controls.append(
+            self.new_paths_view.controls.append(
                 ft.Row(
                     [
                         ft.IconButton(
@@ -139,10 +157,36 @@ class Homepage(ft.Column):
                     ]
                 )
             )
-        self.view_selected_paths.update()
+        self.new_paths_view.update()
 
     def on_remove_new_path(self, e: ft.ControlEvent):
         path = e.control.data
         if path in self.new_paths:
             self.new_paths.remove(path)
             self.render_new_paths()
+
+    def on_add_files(self, e):
+        self.files_picker.pick_files(
+            dialog_title="Add files ...",
+            allow_multiple=True,
+            allowed_extensions=sorted(VIDEO_SUPPORTED_EXTENSIONS),
+        )
+
+    def on_add_folder(self, e):
+        self.folder_picker.get_directory_path(dialog_title="Add folder ...")
+
+    def on_database_change(self, e):
+        self.database_to_load = e.control.value
+
+    def on_change_update(self, e):
+        self.update_on_load = e.control.value
+
+    def on_change_new_name(self, e):
+        self.new_name = e.control.value
+
+    def on_create_database(self, e):
+        pass
+        pass
+
+    def on_open_database(self, e):
+        pass
