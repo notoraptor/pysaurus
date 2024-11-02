20d86660792fc8615736c70d09beebf6036a22bc
notoraptor <notoraptor@users.noreply.github.com>
2024-11-02 09:43:15 -0400

    [videre] Add few tests for container

--- videre_tests/test_container.py
+++ videre_tests/test_container.py
@@ -1,9 +1,46 @@
+import itertools
+
+import pytest
+
 import videre
 from videre.core.sides.padding import Padding
 from videre.windowing.windowfactory import WindowLD
 
 
-def test_container(image_testing):
+@pytest.mark.parametrize(
+    "halign,valign", itertools.product(videre.Alignment, videre.Alignment)
+)
+def test_container_alignments(image_testing, halign, valign):
+    with WindowLD() as window:
+        window.controls = [
+            videre.Container(
+                videre.Text("Hello, World!"),
+                background_color="pink",
+                border=videre.Border.all(2, videre.Colors.blue),
+                horizontal_alignment=halign,
+                vertical_alignment=valign,
+            )
+        ]
+        image_testing(window.snapshot())
+
+
+@pytest.mark.parametrize("padding", [0, 10, 50])
+def test_container_padding(image_testing, padding):
+    with WindowLD() as window:
+        window.controls = [
+            videre.Container(
+                videre.Text("Hello, World!"),
+                background_color="pink",
+                border=videre.Border.all(2, videre.Colors.blue),
+                padding=Padding.axis(padding),
+                vertical_alignment=videre.Alignment.START,
+                horizontal_alignment=videre.Alignment.CENTER,
+            )
+        ]
+        image_testing(window.snapshot())
+
+
+def test_container_too_big_borders(image_testing):
     with WindowLD() as window:
         window.controls = [
             videre.Container(
@@ -21,4 +58,3 @@ def test_container(image_testing):
             )
         ]
         image_testing(window.snapshot())
-
