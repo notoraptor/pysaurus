42c7af9a185fdd31ec895efb53f0edff9da96acd
notoraptor <notoraptor@users.noreply.github.com>
2024-11-10 09:05:48 -0500

    [videre] test text align

--- videre_tests/test_text.py
+++ videre_tests/test_text.py
@@ -8,6 +8,7 @@ from videre.windowing.windowfactory import WindowSD
 
 NB_LINES = (0, 1, 2)
 WRAP = list(videre.TextWrap)
+ALIGN = list(videre.TextAlign)
 
 
 @pytest.mark.parametrize("nb_lines,wrap", itertools.product(NB_LINES, WRAP))
@@ -15,4 +16,14 @@ def test_text(nb_lines, wrap, image_testing):
     lorem_ipsum = ("\n" * nb_lines) + LOREM_IPSUM
     with WindowSD(background=videre.Colors.yellow) as window:
         window.controls = [videre.Text(lorem_ipsum, wrap=wrap)]
+        image_testing(window.snapshot())
+
+
+@pytest.mark.parametrize(
+    "align,wrap",
+    itertools.product([al for al in ALIGN if al != videre.TextAlign.NONE], WRAP),
+)
+def test_text_wrap(align, wrap, image_testing):
+    with WindowSD(background=videre.Colors.yellow) as window:
+        window.controls = [videre.Text(LOREM_IPSUM, wrap=wrap, align=align)]
         image_testing(window.snapshot())
