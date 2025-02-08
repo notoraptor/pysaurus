816684767550755a9242385ad3f0365e68fa4b63
notoraptor <notoraptor@users.noreply.github.com>
2025-02-08 16:22:10 -0500

    [videre] merge more demos

--- videre_tests/antiroom/demo_full.py
+++ videre_tests/antiroom/demo_full.py
@@ -21,18 +21,26 @@ def main():
             buttons=[videre.Button("yes"), videre.Button("NO!")],
         )
 
+    def on_alert(*args):
+        window.alert("You have an alert")
+
+    def on_text_input(*args):
+        work.control = videre.TextInput(weight=1)
+
     button_clear = videre.Button("clear", on_click=clear)
     button_1 = videre.Button(
-        "animator, progress bar and progressing", on_click=on_demo_animator
+        "animator, progressing and progress bar", on_click=on_demo_animator
     )
     button_2 = videre.Button("fancy box", on_click=on_demo_fancybox, square=False)
+    button_3 = videre.Button("alert", on_click=on_alert)
+    button_4 = videre.Button("text input", on_click=on_text_input)
 
     window.controls = [
         videre.Column(
             [
                 videre.Text("Demo of many things", strong=False),
                 videre.Row([button_clear]),
-                videre.Row([button_1, button_2]),
+                videre.Row([button_1, button_2, button_3, button_4]),
                 work,
             ]
         )
@@ -53,7 +61,30 @@ def demo_animator():
 
     aw = videre.Animator(label, on_frame=on_label_frame)
     ap = videre.Animator(pb, on_frame=on_pb_frame, fps=30)
-    return videre.Column([aw, ap, videre.Progressing()])
+    return videre.Column(
+        [
+            aw,
+            ap,
+            videre.Progressing(),
+            videre.ProgressBar(),
+            videre.ProgressBar(),
+            videre.ProgressBar(0.1),
+            videre.ProgressBar(0.5),
+            videre.ProgressBar(0.8),
+            videre.ProgressBar(0.9),
+            videre.ProgressBar(1),
+        ]
+    )
+
+
+def call(args, code):
+    pass
+
+
+def test():
+    x = 1
+    y = 2
+    my_function = call((x, y), "x = y + 2*x")
 
 
 if __name__ == "__main__":
