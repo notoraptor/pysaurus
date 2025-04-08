95e8019fc6e6b45dfa3eb7f92af2d8e8bb14c00b
notoraptor <notoraptor@users.noreply.github.com>
2025-04-08 19:57:49 -0400

    Improve _scan_folder_for_videos(folder:: use stack instead of recursion

--- pysaurus/video/video_file_lister.py
+++ pysaurus/video/video_file_lister.py
@@ -8,26 +8,28 @@ from pysaurus.video import VideoRuntimeInfo
 
 
 def _scan_folder_for_videos(folder: str, files: Dict[AbsolutePath, VideoRuntimeInfo]):
-    entry: os.DirEntry
-    for entry in FileSystem.scandir(folder):
-        if entry.is_dir():
-            _scan_folder_for_videos(entry.path, files)
-        elif (
-            os.path.splitext(entry.name)[1][1:].lower()
-            in constants.VIDEO_SUPPORTED_EXTENSIONS
-        ):
-            # NB: entry.stat()'s field st_dev is set to 0 on Windows.
-            # So, we should better use os.stat().
-            # Reference (2023/04/29, python 3.8):
-            # https://docs.python.org/3/library/os.html#os.DirEntry.stat
-            entry_path = AbsolutePath(entry.path)
-            stat = os.stat(entry_path.path)
-            files[entry_path] = VideoRuntimeInfo.from_keys(
-                size=stat.st_size,
-                mtime=stat.st_mtime,
-                driver_id=stat.st_dev,
-                is_file=True,
-            )
+    stack = [folder]
+    while stack:
+        current_folder = stack.pop()
+        for entry in FileSystem.scandir(current_folder):
+            if entry.is_dir():
+                stack.append(entry.path)
+            elif (
+                os.path.splitext(entry.name)[1][1:].lower()
+                in constants.VIDEO_SUPPORTED_EXTENSIONS
+            ):
+                # NB: entry.stat()'s field st_dev is set to 0 on Windows.
+                # So, we should better use os.stat().
+                # Reference (2023/04/29, python 3.8):
+                # https://docs.python.org/3/library/os.html#os.DirEntry.stat
+                entry_path = AbsolutePath(entry.path)
+                stat = os.stat(entry_path.path)
+                files[entry_path] = VideoRuntimeInfo.from_keys(
+                    size=stat.st_size,
+                    mtime=stat.st_mtime,
+                    driver_id=stat.st_dev,
+                    is_file=True,
+                )
 
 
 def scan_path_for_videos(
