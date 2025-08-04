2ce33d8282f74a51ba79b937085763acdd16fb58
notoraptor <notoraptor@users.noreply.github.com>
2025-08-04 18:11:15 -0400

    [tablify] add doc

--- wip/tablify/core.py
+++ wip/tablify/core.py
@@ -7,6 +7,7 @@ from typing import Any, Iterable
 
 
 def get_slots(cls: type) -> Iterable[str]:
+    """Get slots of a class, including from base classes."""
     return chain.from_iterable(getattr(typ, "__slots__", ()) for typ in cls.__mro__)
 
 
@@ -23,18 +24,32 @@ class IncludeExcludeError(Exception):
 
 
 class AbstractProvider(ABC):
-    def get_field(self, el: Any, field: str) -> Any:
+    """Abstract class to get values and field names from an object or a dictionary."""
+
+    def get_value(self, el: Any, field: str) -> Any:
+        """Get value of a field from given element."""
         raise NotImplementedError()
 
     def get_fields(self, el: Any) -> list[str]:
+        """Get field names from given element."""
         raise NotImplementedError()
 
 
 class ObjectsProvider(AbstractProvider):
-    def get_field(self, el: Any, field: str) -> Any:
+    """Provider for objects other than dictionaries."""
+
+    def get_value(self, el: Any, field: str) -> Any:
         return getattr(el, field)
 
     def get_fields(self, el: Any) -> list[str]:
+        """
+        Get field names from an object.
+
+        Methods looks for field names in the following order:
+        1) looking for class attribute `__props__`
+        2) looking for `__slots__` (including base classes)
+        3) falling back to all public attributes that are not methods.
+        """
         cls = type(el)
         fields = list(getattr(cls, "__props__", ()))
         if not fields:
@@ -50,7 +65,9 @@ class ObjectsProvider(AbstractProvider):
 
 
 class DictProvider(AbstractProvider):
-    def get_field(self, el: dict[str, Any], field: str) -> Any:
+    """Provider for dictionaries."""
+
+    def get_value(self, el: dict[str, Any], field: str) -> Any:
         return el[field]
 
     def get_fields(self, el: dict[str, Any]) -> list[str]:
@@ -67,6 +84,25 @@ def tablify(
     indent: str = "",
     space: int = 2,
 ) -> str:
+    """
+    Convert an iterable of objects or dictionaries into a formatted string table.
+    :param iterable: Iterable of objects or dictionaries.
+    :param include: List of fields to include in the table.
+        If not specified, all fields will be included.
+        Mutually exclusive with `exclude`.
+    :param exclude: List of fields to exclude from the table.
+        Mutually exclusive with `include`.
+    :param index: If not None, adds an index column starting from this value.
+    :param indent: String to prepend to each line of the table.
+    :param space: Number of spaces to add between columns.
+    :return: Formatted string table.
+    :raises IncludeExcludeError: If both include and exclude are specified.
+    :raises NoFieldError:
+        If `include` is not specified and no fields can be determined
+        from the first element.
+    :raises NoFieldAfterExcludingError:
+        If `exclude` is specified and no fields are left after excluding.
+    """
     if include and exclude:
         raise IncludeExcludeError()
 
@@ -75,21 +111,19 @@ def tablify(
     iterator = iter(iterable)
     try:
         el = next(iterator)
-        if isinstance(el, dict):
-            provider = DictProvider()
-        else:
-            provider = ObjectsProvider()
-
+        provider = DictProvider() if isinstance(el, dict) else ObjectsProvider()
         fields = fields or provider.get_fields(el)
         if not fields:
             raise NoFieldError()
         if exclude:
+            if not isinstance(exclude, set):
+                exclude = set(exclude)
             fields = [field for field in fields if field not in exclude]
         if not fields:
             raise NoFieldAfterExcludingError()
 
         while True:
-            rows.append([str(provider.get_field(el, field)) for field in fields])
+            rows.append([str(provider.get_value(el, field)) for field in fields])
             el = next(iterator)
     except StopIteration:
         if not rows:
