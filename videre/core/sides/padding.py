19852fc2b744d93bfda5dbcb07af5a088c72d8c3
notoraptor <notoraptor@users.noreply.github.com>
2025-08-24 18:12:31 -0400

    Move videre to a separate repository

--- videre/core/sides/padding.py
+++
@@ -1,5 +0,0 @@
-from videre.core.sides.margin import Margin
-
-
-class Padding(Margin):
-    __slots__ = ()
--- videre/core/utils.py
+++
@@ -1,9 +0,0 @@
-import threading
-
-
-def launch_thread(function, *args, **kwargs):
-    thread = threading.Thread(
-        target=function, args=args, kwargs=kwargs, name=function.__name__
-    )
-    thread.start()
-    return thread
