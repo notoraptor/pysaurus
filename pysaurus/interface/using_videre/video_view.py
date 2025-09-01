b90b0a569ca50f82853e486bb611426149e2a750
notoraptor <notoraptor@users.noreply.github.com>
2025-09-01 12:58:27 -0400

    pysaurus+videre: update

--- pysaurus/interface/using_videre/video_view.py
+++ pysaurus/interface/using_videre/video_view.py
@@ -1,9 +1,13 @@
+from typing import Callable
+
 import videre
 
 from pysaurus.core import notifications
 from pysaurus.interface.using_videre.backend import get_backend
 from pysaurus.properties.properties import PropUnitType
 from pysaurus.video.video_pattern import VideoPattern
+
+LIGHT_GREY = videre.parse_color((240, 240, 240))
 
 
 class VideoPropertyView(videre.Column):
@@ -23,17 +27,11 @@ class VideoAttributesView(videre.Column):
 
     def __init__(self, video: VideoPattern, **kwargs):
         checkbox = videre.Checkbox()
+        properties = video.properties
         self._video = video
         self._menu = videre.ContextButton(
             " \u2630 ",
-            actions=[
-                (
-                    f"Mark as {'unwatched' if video.watched else 'watched'}",
-                    self._change_watched,
-                ),
-                ("Open file", self._open_file),
-                ("Open containing folder", self._open_containing_folder),
-            ],
+            actions=self._get_menu_actions(video),
             square=True,
         )
         self._text_path = videre.Text(
@@ -86,20 +84,45 @@ class VideoAttributesView(videre.Column):
                     f"Subtitles: {', '.join(video.subtitle_languages or ['(none)'])}"
                 ),
                 videre.Text(f"Similarity: {video.similarity}"),
-            ],
+            ]
+            + (
+                [
+                    videre.Text("PROPERTIES", strong=True),
+                    *(
+                        videre.Row(
+                            [
+                                videre.Text(f"{name}:", strong=True),
+                                *(
+                                    videre.Container(
+                                        videre.Text(str(value), italic=True),
+                                        background_color=LIGHT_GREY,
+                                        padding=videre.Padding.axis(2, 4),
+                                    )
+                                    for value in values
+                                ),
+                            ],
+                            space=5,
+                            vertical_alignment=videre.Alignment.CENTER,
+                        )
+                        for name, values in properties.items()
+                    ),
+                ]
+                if properties
+                else []
+            ),
             space=2,
             **kwargs,
         )
 
-    def _open_file(self):
+    def _action_open_file(self):
         get_backend(self).open_video(self._video.video_id)
         self.get_window().notify(notifications.Message("Opened:", self._video.filename))
 
-    def _open_containing_folder(self):
+    def _action_open_containing_folder(self):
         ret = get_backend(self).open_containing_folder(self._video.video_id)
         self.get_window().notify(notifications.Message("Opened folder:", ret))
 
-    def _change_watched(self):
+    def _action_change_watched(self):
         backend = get_backend(self)
         watched = backend.mark_as_read(self._video.video_id)
         video = backend.get_video(self._video.video_id)
@@ -109,13 +132,16 @@ class VideoAttributesView(videre.Column):
             videre.Colors.lightgray if video.watched else videre.Colors.blue
         )
         self._text_path.strong = watched
-        self._menu.actions = [
+        self._menu.actions = self._get_menu_actions(video)
+
+    def _get_menu_actions(self, video: VideoPattern) -> list[tuple[str, Callable]]:
+        return [
             (
                 f"Mark as {'unwatched' if video.watched else 'watched'}",
-                self._change_watched,
+                self._action_change_watched,
             ),
-            ("Open file", self._open_file),
-            ("Open containing folder", self._open_containing_folder),
+            ("Open file", self._action_open_file),
+            ("Open containing folder", self._action_open_containing_folder),
         ]
 
 
@@ -131,15 +157,8 @@ class VideoView(videre.Container):
             width=300,
             horizontal_alignment=videre.Alignment.CENTER,
         )
-        properties = videre.Column(
-            [
-                VideoPropertyView(name, values)
-                for name, values in video.properties.items()
-                if values
-            ]
-        )
         attributes = VideoAttributesView(video, weight=1)
-        layout = videre.Row([thumbnail, properties, attributes], space=2)
+        layout = videre.Row([thumbnail, attributes], space=6)
         super().__init__(layout, padding=videre.Padding.axis(vertical=10))
         if index % 2 == 1:
             self.background_color = self.__BACKGROUND_EVEN__
