081a185df50f33e105b326b66103ac12398f064e
notoraptor <notoraptor@users.noreply.github.com>
2025-09-15 11:47:05 -0400

    pysaurus+videre: clean up code

--- pysaurus/interface/using_videre/_demo.py
+++ pysaurus/interface/using_videre/_demo.py
@@ -1,9 +1,50 @@
+import inspect
+
 import videre
 
+from pysaurus.interface.using_videre.constants import (
+    FIELD_MAP,
+    FieldInfo,
+    FieldType,
+    GroupPerm,
+    Uniconst,
+)
 from pysaurus.interface.using_videre.path_set_view import PathSetView
+from pysaurus.video.video_pattern import VideoPattern
 
 
-def main():
+def get_prop_info(
+    prop: property, title: str | None = None, permission: GroupPerm = GroupPerm.ALL
+) -> FieldInfo:
+    name = prop.__name__
+    sig = inspect.signature(prop.fget)
+    ret_type = sig.return_annotation
+    if ret_type in (bool, int, float, str):
+        field_type = getattr(FieldType, ret_type.__name__.upper())
+    else:
+        if ret_type.__lt__ is object.__lt__:
+            field_type = FieldType.UNSORTABLE
+        else:
+            field_type = FieldType.SORTABLE
+    return FieldInfo(name, title, permission, field_type)
+
+
+def main_uniconst():
+    for name in dir(Uniconst):
+        if not name.startswith("_"):
+            print(name, getattr(Uniconst, name))
+
+
+def main_try_get_prop_info():
+    for field in FIELD_MAP.fields.values():
+        guessed = get_prop_info(getattr(VideoPattern, field.name))
+        if guessed != field:
+            print("DIFF", field.name)
+            print(f"\tFIELD\t{field}")
+            print(f"\tGUESS\t{guessed}")
+
+
+def main_try_videre():
     window = videre.Window()
     window.controls = [
         videre.Column(
@@ -15,4 +56,4 @@ def main():
 
 
 if __name__ == "__main__":
-    main()
+    main_try_get_prop_info()
--- pysaurus/interface/using_videre/uniconst.py
+++
@@ -1,6 +0,0 @@
-from pysaurus.interface.using_videre.constants import Uniconst
-
-if __name__ == "__main__":
-    for name in dir(Uniconst):
-        if not name.startswith("_"):
-            print(name, getattr(Uniconst, name))
