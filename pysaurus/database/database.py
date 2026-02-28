0a180bed7307ebe184ba95e621b2ddf8d538d6d6
notoraptor <stevenbocco@gmail.com>
2026-02-28 05:53:40 -0500

    Start real migration to SQL

--- pysaurus/database/database.py
+++ pysaurus/database/database.py
@@ -1,6 +1,6 @@
 from pysaurus.database.jsdb.json_database import JsonDatabase
 from saurus.sql.pysaurus_collection import PysaurusCollection
 
-USE_SQL = False
+USE_SQL = True
 BaseDatabase = PysaurusCollection if USE_SQL else JsonDatabase
 Database = BaseDatabase
